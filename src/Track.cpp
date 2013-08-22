8a338991f02a4a4c6fa43c24eed653cc5d3accc7
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2013-08-23 02:37:22 +0300

    color init fixed

--- src/Track.cpp
+++ src/Track.cpp
@@ -19,7 +19,7 @@ Track::~Track(){
 
 
 //--------------------------------------------------------------
-void Track::setup(int id, const ofRectangle &bb, int cols){
+void Track::setup(int id, const ofRectangle &bb, int cols, const ofColor &color){
     
     trackId = id;
     columns = cols;
@@ -29,7 +29,7 @@ void Track::setup(int id, const ofRectangle &bb, int cols){
 	
     cells.assign(cols, TrackCell());
 	for(int i = 0; i<columns; i++){
-        cells[i].setup(ofRectangle(buttonWidth * i, bb.getY(), buttonWidth, trackHeight), i);
+        cells[i].setup(ofRectangle(buttonWidth * i, bb.getY(), buttonWidth, trackHeight), i, color);
 	}
     
     cellStates.assign(columns, bool(false));
